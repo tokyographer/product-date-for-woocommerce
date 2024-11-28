307cb8c7928b8ee7b65087d14485f9e6f06a8686
Anthony T <tokyographer@gmail.com>
2024-11-28 22:07:27 +0100

    created js file

---
+++ js/quick-edit.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function ($) {
+    // Populate Quick Edit fields
+    $('a.editinline').on('click', function () {
+        const postId = $(this).closest('tr').attr('id').replace('post-', '');
+        const retreatStartDate = $(`#retreat_start_date_${postId}`).text();
+
+        // Populate the Retreat Start Date field
+        $('input[name="_retreat_start_date"]').val(retreatStartDate || '');
+    });
+});
